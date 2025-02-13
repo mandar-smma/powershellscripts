5ea58a907b3c5c2f8cd40a94bb8102c0ae793e40
mandhar30 <mandhar30@gmail.com>
2025-02-13 17:52:44 +0100

    copy permission access policies

--- azureAD/copy_policiesto_tenant.ps1
+++ azureAD/copy_policiesto_tenant.ps1
@@ -4,6 +4,9 @@ Connect-MgGraph -TenantId "<source-tenant-id>" #example production tenant
 
 # Export policies from source tenant
 $policies = Get-MgConditionalAccessPolicy
+# OR Get all the Permission Grant Policies
+# $policies = Get-MgDirectoryRoleDefinition
+
 $policies | ConvertTo-Json | Out-File -FilePath "Policies.json"
 
 # Connect to target tenant
@@ -14,4 +17,5 @@ $importedPolicies = Get-Content -Path "Policies.json" | ConvertFrom-Json
 foreach ($policy in $importedPolicies) {
     # Use the policy details to create new policies in the target tenant
     New-MgConditionalAccessPolicy -DisplayName $policy.DisplayName -State $policy.State -Conditions $policy.Conditions -GrantControls $policy.GrantControls
+    # New-MgDirectoryRoleDefinition -DisplayName $policy.DisplayName -Description $policy.Description
 }
