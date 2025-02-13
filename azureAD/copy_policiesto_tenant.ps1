e71ff7f12c3f387dc8f30d97c9f0094c2bce1323
mandhar30 <mandhar30@gmail.com>
2025-02-13 17:45:42 +0100

    copy conditional access policies to another tenant

---
+++ azureAD/copy_policiesto_tenant.ps1
@@ -0,0 +1,17 @@
+#Copying Conditional Access policies from one Azure AD tenant to another via PowerShell
+# Connect to source tenant
+Connect-MgGraph -TenantId "<source-tenant-id>" #example production tenant
+
+# Export policies from source tenant
+$policies = Get-MgConditionalAccessPolicy
+$policies | ConvertTo-Json | Out-File -FilePath "Policies.json"
+
+# Connect to target tenant
+Connect-MgGraph -TenantId "<target-tenant-id>" #example test tenant
+
+# Import and recreate policies in target tenant
+$importedPolicies = Get-Content -Path "Policies.json" | ConvertFrom-Json
+foreach ($policy in $importedPolicies) {
+    # Use the policy details to create new policies in the target tenant
+    New-MgConditionalAccessPolicy -DisplayName $policy.DisplayName -State $policy.State -Conditions $policy.Conditions -GrantControls $policy.GrantControls
+}
